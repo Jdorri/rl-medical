30c03114841f2ce82a0947344d182e4e4c191329
Maleakhi Agung Wijaya <maleakhiw@pop-os.localdomain>
2020-02-11 14:39:02 +0000

    modify tutorial

--- pyqt_tutorial/multithreading.py
+++ pyqt_tutorial/multithreading.py
@@ -4,10 +4,17 @@ from PyQt5.QtGui import *
 import sys
 import time
 
+def hello_world():
+    print("hello world")
+
 class MyThread(QThread):
     change_value = pyqtSignal(int)
+    def __init__(self, func):
+        super().__init__()
+        self.func = func
 
     def run(self):
+        self.func()
         count = 0
         while count < 100:
             count += 1
@@ -25,6 +32,7 @@ class Window(QDialog):
         self.top = 200
         self.width = 300
         self.height = 100
+        self.thread = None
 
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
@@ -47,7 +55,8 @@ class Window(QDialog):
         self.setLayout(vbox)
     
     def startProgressVar(self):
-        self.thread = MyThread()
+        print(viewer.thread)
+        self.thread = MyThread(hello_world)
         self.thread.change_value.connect(self.setProgressVal)
 
         self.thread.start()
@@ -58,5 +67,7 @@ class Window(QDialog):
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     viewer = Window()
+    viewer.thread = "haha"
+    print(viewer.thread)
     sys.exit(app.exec_())
 
