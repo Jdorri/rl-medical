9a8def2c9440f166684a5e80d13250b466ca1a70
ollenilsson19 <ollenilsson@hotmail.se>
2020-04-11 12:06:12 +0100

    Add GPU cluster submission scripts for HITL

---
+++ transfer.py
@@ -0,0 +1,22 @@
+import subprocess
+import glob
+
+subprocess.run(["scp", "-pr", "./call_gpu_copy.py", "USER@gpucluster.doc.ic.ac.uk:Documents/rl-medical"])
+
+files = glob.glob("./examples/LandmarkDetection/DQN/*.py")
+for file in files:
+    subprocess.run(["scp", "-pr", file, "USER@gpucluster.doc.ic.ac.uk:Documents/rl-medical/examples/LandmarkDetection/DQN"])
+
+
+
+
+
+sshProcess = subprocess.Popen(['ssh',
+                               "USER@gpucluster.doc.ic.ac.uk"],
+                               stdin=subprocess.PIPE,
+                               stdout = subprocess.PIPE,
+                               universal_newlines=True,
+                               bufsize=0)
+
+sshProcess.stdin.write(f"python3 Documents/rl-medical/call_gpu_copy.py\n")
+sshProcess.stdin.close()
