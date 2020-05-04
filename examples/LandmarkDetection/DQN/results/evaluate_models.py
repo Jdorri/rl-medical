1ec698c60d40f917e8a25b8c84ceeeac5404f3bd
Faidon Mitzalis <phaedonmit@yahoo.gr>
2020-05-04 22:39:02 +0100

    new path

--- examples/LandmarkDetection/DQN/results/evaluate_models.py
+++ examples/LandmarkDetection/DQN/results/evaluate_models.py
@@ -17,7 +17,7 @@ csvfile = f"results/eval_logs/log_eval_{data_type}_{time.time()}.csv"
 if imperial_cluster:
     redir = '/vol/biomedic/users/aa16914/shared/data/RL_data/' 
     # model = "/volumes/project/2019/545/g1954503/oen19/LandmarkDetection/001/output/001/model-600000"
-    directory = "/vol/project/2019/545/g1954503/hgc19/LandmarkDetection/CardiacMRI/003/output/CardiacMRI003/" 
+    directory = "/vol/project/2019/545/g1954503/fm1710/LandmarkDetection/CardiacMRI/002/output/CardiacMRI002/" 
 else:
     redir = 'data/filenames/'
     # model = "/volumes/project/2019/545/g1954503/oen19/LandmarkDetection/001/output/001/model-600000"
