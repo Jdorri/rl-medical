083685eb23abcf758ed34ac7ea524be0f5c51c14
Maleakhi Agung Wijaya <keystorm7@gmail.com>
2020-05-07 17:44:21 +0100

    Finish application help, just need more text on it

--- examples/LandmarkDetection/DQN/application_help.py
+++ examples/LandmarkDetection/DQN/application_help.py
@@ -8,65 +8,110 @@ class ApplicationHelp(QWidget):
 
     def __init__(self):
         super(ApplicationHelp, self).__init__()
+        # Close help window with shortcut
+        self.shortcut_close = QShortcut(QKeySequence('Ctrl+Q'), self)
+        self.shortcut_close.activated.connect(self.close)
 	    
+        ## Left settings on help window
         self.leftlist = QListWidget()
-        self.leftlist.insertItem(0, 'Contact')
-        self.leftlist.insertItem(1, 'Personal')
-        self.leftlist.insertItem(2, 'Educational')
+        self.leftlist.insertItem(0, 'Welcome')
+        self.leftlist.insertItem(1, 'Automatic Mode')
+        self.leftlist.insertItem(2, 'Browse Mode')
+
+        # Initialise respective widgets
+        self.welcome_stack = QWidget()
+        self.automatic_stack = QWidget()
+        self.browse_stack = QWidget()
+
+        self.welcome_UI()
+        self.automatic_UI()
+        self.browse_UI()
 		
-        self.stack1 = QWidget()
-        self.stack2 = QWidget()
-        self.stack3 = QWidget()
-		
-        self.stack1UI()
-        self.stack2UI()
-        self.stack3UI()
-		
+        # Stack (tabbing)
         self.Stack = QStackedWidget(self)
-        self.Stack.addWidget(self.stack1)
-        self.Stack.addWidget(self.stack2)
-        self.Stack.addWidget(self.stack3)
-		
+        self.Stack.addWidget(self.welcome_stack)
+        self.Stack.addWidget(self.automatic_stack)
+        self.Stack.addWidget(self.browse_stack)
+
+        # Manage overall widget layout
         hbox = QHBoxLayout(self)
         hbox.addWidget(self.leftlist)
         hbox.addWidget(self.Stack)
-
         self.setLayout(hbox)
+
+        # Event handler
         self.leftlist.currentRowChanged.connect(self.display)
-        self.resize(1300, 800)
+
+        # Responsive design tools
+        self.leftlist.setMaximumWidth(300)
+        self.resize(1000, 800)
         self.center()
         self.setWindowTitle('Application Help')
-        self.show()
+        
+        self.setStyleSheet("""
+        QWidget {
+            background: white;
+        }
+        QListWidget {
+            background: #EBEEEE;
+            font-size: 14px;
+            padding: 5px;
+        }
+        QPlainTextEdit {
+            border: none;
+        }
+        """)
     
     def center(self):
+        """
+        Force widget to be on the center
+        """
         qr = self.frameGeometry()
         cp = QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
         self.move(qr.topLeft())
 		
-    def stack1UI(self):
-        layout = QFormLayout()
-        layout.addRow("Name",QLineEdit())
-        layout.addRow("Address",QLineEdit())
-        #self.setTabText(0,"Contact Details")
-        self.stack1.setLayout(layout)
+    def welcome_UI(self):
+        self.welcome_text = QPlainTextEdit(self)
+        self.welcome_text.setReadOnly(True)
+        hbox_layout = QHBoxLayout()
+        hbox_layout.addWidget(self.welcome_text)
+
+        self.welcome_text.appendHtml("""
+        <h1 style='color:#003E74'> Welcome </h1>
+        <br /><br />
+        <p>Text here</p>
+        """)
+
+        self.welcome_stack.setLayout(hbox_layout)
+
+    def automatic_UI(self):
+        self.automatic_text = QPlainTextEdit(self)
+        self.automatic_text.setReadOnly(True)
+        hbox_layout = QHBoxLayout()
+        hbox_layout.addWidget(self.automatic_text)
+
+        self.automatic_text.appendHtml("""
+        <h1 style='color:#003E74'> Automatic Mode Help </h1>
+        <br /><br />
+        <p>Text here</p>
+        """)
+
+        self.automatic_stack.setLayout(hbox_layout)
 		
-    def stack2UI(self):
-        layout = QFormLayout()
-        sex = QHBoxLayout()
-        sex.addWidget(QRadioButton("Male"))
-        sex.addWidget(QRadioButton("Female"))
-        layout.addRow(QLabel("Sex"),sex)
-        layout.addRow("Date of Birth",QLineEdit())
-		
-        self.stack2.setLayout(layout)
-		
-    def stack3UI(self):
-        layout = QHBoxLayout()
-        layout.addWidget(QLabel("subjects"))
-        layout.addWidget(QCheckBox("Physics"))
-        layout.addWidget(QCheckBox("Maths"))
-        self.stack3.setLayout(layout)
+    def browse_UI(self):
+        self.browse_text = QPlainTextEdit(self)
+        self.browse_text.setReadOnly(True)
+        hbox_layout = QHBoxLayout()
+        hbox_layout.addWidget(self.browse_text)
+
+        self.browse_text.appendHtml("""
+        <h1 style='color:#003E74'> Browse Mode Help </h1>
+        <br /><br />
+        <p>Text here</p>
+        """)
+
+        self.browse_stack.setLayout(hbox_layout)
 		
     def display(self,i):
         self.Stack.setCurrentIndex(i)
